7301d5b76d0963fec20758ceb50a846f5db69947
Houston Golden <houstongolden@gmail.com>
2014-04-26 19:18:56 -0700

    rails_apps_composer: initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Busk::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Busk::Application.config.secret_key_base = 'ab5fbeea5caf0ccd7808c7ee3ca912238f2245c1b0636d5ad7dd6d816ce42eccd9dc4f674b1ebfcf7c945f655b0d58977aefbd17ebcfbbc361df61f3256a72a7'
